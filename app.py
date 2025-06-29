71812643af64ba619f9370695fcd4ade9ebaf096
c-azubuike <155354205+c-azubuike@users.noreply.github.com>
2025-06-29 14:52:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ nltk.download("punkt")
 nltk.download('punkt_tab')
 nltk.download("stopwords")
 nltk.download("wordnet")
-nltk.download("averaged_perceptron_tagger")
+nltk.download("averaged_perceptron_tagger_eng")
 
 from preprocessing import fast_preprocess
 
