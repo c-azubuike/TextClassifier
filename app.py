28c4d16e578e6e0d2ca1d9ebb925135c3c5389ff
c-azubuike <155354205+c-azubuike@users.noreply.github.com>
2025-06-29 14:43:55 -0700

    Updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 # app.py
 import streamlit as st
 import joblib
+
+nltk.download("punkt")
+nltk.download("stopwords")
+nltk.download("wordnet")
+nltk.download("averaged_perceptron_tagger")
+
 from preprocessing import fast_preprocess
 
 # Load model
